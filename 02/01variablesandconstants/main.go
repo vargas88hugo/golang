f12b41f7fd502395c51098bedb5ba28ec8992e25
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-16 21:46:48 -0500

    [Feature] Added variable exercises, it is fun!

---
+++ 02/01variablesandconstants/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+     /*
+     var firstName, lastName string
+     firstName = "Hugo"
+     lastName = "Vargas"
+     */
+
+     firstName, lastName := "Hugo", "Vargas"
+
+     firstName = "Mairelys"
+
+     fmt.Println(firstName, lastName)
+}
