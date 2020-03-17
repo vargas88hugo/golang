27a86097d364631537baef062406e346ff2d26e3
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-16 23:06:50 -0500

    [Feature] Created new file that implements switch statements

---
+++ 03/02switch/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	a = 2
+
+	switch a {
+	case 1:
+		fmt.Println("The value is 1")
+		break
+	case 2:
+		fmt.Println("The value is 2")
+		break
+	case 3:
+		fmt.Println("The value is 3")
+	default:
+		fmt.Println("Default!!!")
+	}
+
+	switch a {
+	case 1:
+		fallthrough
+	case 2:
+		fallthrough
+	case 3:
+		fallthrough
+	case 4:
+		fallthrough
+	case 5:
+		fmt.Println("You are during the week")
+	case 6:
+		fallthrough
+	case 7:
+		fmt.Println("You are during the weekend!!!")
+	default:
+		fmt.Println("Not is a valid day")
+	}
+
+	switch b := 6; {
+	case b >= 0 && b <= 5:
+		fmt.Println("You are during the week")
+	case b >= 6 && b <= 7:
+		fmt.Println("You are during the weekend!!!")
+	default:
+		fmt.Println("Not is a valid day")
+	}
+}
