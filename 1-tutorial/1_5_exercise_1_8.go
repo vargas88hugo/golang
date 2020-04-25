f962075052e15b9537de7dec74321e75657149ac
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-25 13:54:18 -0500

    [Feature] Completed exercise 1.5

---
+++ 1-tutorial/1_5_exercise_1_8.go
@@ -0,0 +1,36 @@
+// Fetch prints the content found at a URL with io.Copy
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		fmt.Println("\nStatus: " + strconv.Itoa(resp.StatusCode))
+
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		}
+	}
+}
