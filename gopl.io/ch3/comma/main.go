48b28a9e0b43eb5b95a15e4de6e384f29543774c
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-28 15:24:16 -0500

    [Feature] Implemented exercise that adds a comma every three characters from the end of the string

---
+++ gopl.io/ch3/comma/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		fmt.Println(comma(input.Text()))
+	}
+}
+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
